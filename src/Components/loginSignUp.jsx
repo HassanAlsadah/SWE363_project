3451e04f2b9b8e1b58fb13bea2a2008a843d1e2c
Mohammed Al Lail <alalmoh3404@gmail.com>
2025-04-14 00:45:22 +0300

    Finishing the login screen

--- src/Components/loginSignUp.jsx
+++ src/Components/loginSignUp.jsx
@@ -1,11 +1,63 @@
 import React from "react";
+import "./loginSignup.css";
+import googlImg from "./Assets/Google.webp";
+import lockImg from "./Assets/lock.png";
+import personImg from "./Assets/person.png"
 
 
 function LoginPage() {
 
     return(
-        <div>
-            <h1>Hello Mohammed</h1>
+        
+        <div className="container">
+            
+            <div className="header">
+
+                <div className="text">SignUp</div>
+                <div className="underline"></div>
+            </div>
+
+            <div className="inputs">
+
+           
+                <div className="input">
+                <img src={personImg} alt="logo" />
+                    <input type="email" placeholder="Example@gmail.com" />
+                </div>
+
+          
+
+                <div className="input">
+                    <img src={lockImg} alt="logo" />
+                    <input type="password" placeholder="Password" />
+                </div>
+
+                
+
+            </div>
+
+            <div className="login-btn"><h3>Login</h3></div>
+
+            <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
+
+            <hr />
+
+            <div className="submit-container">
+
+                <div className="submit">
+                <img src={googlImg} alt="logo" />
+                    Continue with Google
+
+                </div>
+
+                <h3>OR</h3>
+
+                <div className="submit">Craet new Account</div>
+
+                
+            </div>
+            
+              
         </div>
     )
 }
