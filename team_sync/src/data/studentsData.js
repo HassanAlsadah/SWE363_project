832e311c776994b9800cacb6dc223173c1d8c251
HussainALIbrahim <161192439+HussainALIbrahim@users.noreply.github.com>
2025-04-17 18:34:34 +0300

    Add files via upload

---
+++ team_sync/src/data/studentsData.js
@@ -0,0 +1,8 @@
+// studentsData.js
+export const students = [
+    { id: 1, name: "John Doe", email: "john@example.com" },
+    { id: 2, name: "Jane Smith", email: "jane@example.com" },
+    { id: 3, name: "Mike Johnson", email: "mike@example.com" },
+    { id: 4, name: "Sarah Williams", email: "sarah@example.com" },
+    { id: 5, name: "David Brown", email: "david@example.com" },
+  ];
